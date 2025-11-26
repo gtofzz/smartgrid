532261d84b664eb37994da5295606e0f89bc9252
gtofzz <gustavofigueiredo@alunos.utfpr.edu.br>
2025-11-26 04:23:32 -0300

    Add MQTT and I2C test clients

---
+++ servermqtt/config.h
@@ -0,0 +1,14 @@
+#pragma once
+
+// MQTT broker configuration
+#define BROKER_HOST "localhost"
+#define BROKER_PORT 1883
+
+// Group identification
+#define GROUP_ID_NUM 3
+#define GROUP_ID_STR "3"
+
+// MQTT topics
+#define TOPIC_PWM "grupo3/pwm"
+#define TOPIC_SENSORS "grupo3/sensores"
+#define TOPIC_STATUS "grupo3/status"
